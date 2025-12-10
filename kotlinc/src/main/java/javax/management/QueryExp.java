6b134b67b2ab6795be711558e84868c684f7a4c1
Wadamzmail <alksndrmetwakil@gmail.com>
2025-12-10 13:02:02 +0200

    1

--- kotlinc/src/main/java/javax/management/QueryExp.java
+++
@@ -1,9 +0,0 @@
-package javax.management;
-
-import java.io.Serializable;
-
-public interface QueryExp extends Serializable {
-    boolean apply(ObjectName var1) throws BadStringOperationException, BadBinaryOpValueExpException, BadAttributeValueExpException, InvalidApplicationException;
-
-    void setMBeanServer(MBeanServer var1);
-}
--- kotlinc/src/main/java/javax/management/loading/ClassLoaderRepository.java
+++
@@ -1,9 +0,0 @@
-package javax.management.loading;
-
-public interface ClassLoaderRepository {
-    Class<?> loadClass(String var1) throws ClassNotFoundException;
-
-    Class<?> loadClassWithout(ClassLoader var1, String var2) throws ClassNotFoundException;
-
-    Class<?> loadClassBefore(ClassLoader var1, String var2) throws ClassNotFoundException;
-}
