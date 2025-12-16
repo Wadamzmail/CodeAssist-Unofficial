a2d957144f272c05fdeef9d97e9eae76bbe67ee7
Wadamzmail <alksndrmetwakil@gmail.com>
2025-12-16 22:28:07 +0200

    1

--- app/src/main/java/com/tyron/code/language/CachedAutoCompleteProvider.kt
+++ app/src/main/java/com/tyron/code/language/CachedAutoCompleteProvider.kt
@@ -39,7 +39,7 @@ class CachedAutoCompleteProvider(
                 editor.currentFile,
                 line,
                 column,
-                result.getOrThrow().prefix,
+                result.getOrThrow().completionPrefix,
                 result.getOrThrow()
             )
         } else {
@@ -49,7 +49,7 @@ class CachedAutoCompleteProvider(
         return result.getOrNull()
     }
 
-    override fun getPrefix(editor: Editor?, line: Int, column: Int): String {
+    override fun getPrefix(editor: Editor, line: Int, column: Int): String {
         return provider.getPrefix(editor, line, column)
     }
 
