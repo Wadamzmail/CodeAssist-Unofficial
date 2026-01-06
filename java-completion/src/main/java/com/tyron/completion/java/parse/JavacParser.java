75c19e83eb69047d95c66e7e9c2514fe0720edde
Wadamzmail <alksndrmetwakil@gmail.com>
2026-01-06 07:33:42 +0200

    1﷼

--- java-completion/src/main/java/com/tyron/completion/java/parse/JavacParser.java
+++ java-completion/src/main/java/com/tyron/completion/java/parse/JavacParser.java
@@ -53,8 +53,9 @@ public class JavacParser {
         "-bootclasspath",
         joinPath(Arrays.asList(BuildModule.getAndroidJar(), BuildModule.getLambdaStubs())));
 //TODO : fix duplicate class issue in java17
-//Collections.addAll(options, "-target", "17", "-source", "17");       
-    Collections.addAll(options, "-target", "1.8", "-source", "1.8");
+    Collections.addAll(options, "--release", "17");
+    Collections.addAll(options, "-target", "17", "-source", "17");       
+//    Collections.addAll(options, "-target", "1.8", "-source", "1.8");
     Collections.addAll(options, "-cp", joinPath(cpInfo));
 
     JavacTool tool = JavacTool.create();
