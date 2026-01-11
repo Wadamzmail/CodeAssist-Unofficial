dfcaa7bde213c3b460340ba464decabb3358c468
Wadamzmail <alksndrmetwakil@gmail.com>
2026-01-11 07:48:32 +0200

    1

--- app/src/main/java/com/tyron/code/ui/editor/impl/text/rosemoe/CodeEditorFragment.java
+++ app/src/main/java/com/tyron/code/ui/editor/impl/text/rosemoe/CodeEditorFragment.java
@@ -101,6 +101,7 @@ import org.apache.commons.vfs2.FileSystemManager;
 import org.apache.commons.vfs2.VFS;
 import org.jetbrains.kotlin.com.intellij.util.ReflectionUtil;
 import javax.tools.JavaFileObject;
+import com.sun.tools.javac.util.JCDiagnostic;
 
 // import com.tyron.editor.Content;
 
@@ -207,10 +208,11 @@ public class CodeEditorFragment extends Fragment
 
     ServiceLoader<DiagnosticProvider> providers = ServiceLoader.load(DiagnosticProvider.class);
     for (DiagnosticProvider provider : providers) {
-      List<? extends Diagnostic<? extends JavaFileObject>> diagnostics = provider.getDiagnostics(module, mCurrentFile);
+   //   List<? extends Diagnostic<?>> diagnostics = provider.getDiagnostics(module, mCurrentFile);
+     List<JCDiagnostic> diagnostics = provider.getDiagnostics(module, mCurrentFile);
 
       mEditor.setDiagnostics(
-          diagnostics.stream().map(DiagnosticWrapper::new).collect(Collectors.toList()));
+          diagnostics.stream().map(DiagnosticWrapper::new).collect(Collectors.toList()));     
     }
   }
 
