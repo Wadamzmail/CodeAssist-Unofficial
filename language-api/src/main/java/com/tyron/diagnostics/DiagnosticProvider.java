1a0c64df78ec7bb55711bcdd6ff67b8d2306e30d
Wadamzmail <alksndrmetwakil@gmail.com>
2026-01-11 09:01:05 +0200

    1

--- language-api/src/main/java/com/tyron/diagnostics/DiagnosticProvider.java
+++ language-api/src/main/java/com/tyron/diagnostics/DiagnosticProvider.java
@@ -4,9 +4,11 @@ import com.tyron.builder.project.api.Module;
 import java.io.File;
 import java.util.List;
 import javax.tools.Diagnostic;
+import com.sun.tools.javac.util.JCDiagnostic;
 
 /** Implementations may provide their own diagnostics for the specified file */
 public interface DiagnosticProvider {
 
-  List<? extends Diagnostic<?>> getDiagnostics(Module module, File file);
+ // List<? extends Diagnostic<?>> getDiagnostics(Module module, File file);
+ List<JCDiagnostic> getDiagnostics(Module module, File file);
 }
